78fb57e05dd89936fd70b3b64ca409e5d855f812
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-02-21 10:23:15 -0800

    google analytics test

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
 	initializePage();
 });
 
+function onClicks(event){
+	gtag("send", "event", 'like', 'click');
+	console.log("FUCK");
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+	$(".likeCtr").click(onClicks);
 }
+
